2d6483fdf5384261592e61cb4aa6f90f030df65c
Kxyiv <anthonybivin6@gmail.com>
2022-05-18 18:17:18 -0600

    Added to HTML and Js

--- script.js
+++ script.js
@@ -1,61 +1,75 @@
-const quizBody = document.getElementById('#quiz')
-const quizResults = document.getElementById('results')
-const submitQuiz = document.getElementById('submit')
+const quizBody = document.getElementById("#quiz");
+const quizResults = document.getElementById("#results");
+const submitQuiz = document.getElementById("#submit");
+const startQuiz = document.querySelector(".startBtn");
+const quiz = document.querySelector("#quiz");
+const introduction = document.querySelector(".introduction");
+const answerButtons = document.querySelectorAll(".answerButtons");
+let currentQuestionIndex = 0;
+const question = document.querySelector(".question");
 
+const questions = [
+  {
+    question: "How many data-types are there?",
+    answers: [
+      "2",
+      "5",
+      "4",
+    ],
+    correctAnswer: "5",
+  },
+  {
+    question: "What is the true/false data-type named?",
+    answers: [
+      "String",
+      "Boolean",
+      "Number",
+    ],
+    correctAnswer: "Boolean",
+  },
+  {
+    question: "The condition in an if/else statement is enclosed with a _____.",
+    answers: [
+      "Curly Brackets",
+      "Parenthesis",
+      "Square Brackets",
+],
+    correctAnswer: "Curly Brackets",
+  },
+  {
+    question:
+      "Which method outputs a message to the web console and is also used for debugging?",
+    answers: [
+      "console.bug",
+      "console.var",
+      "console.log",
+    ],
+    correctAnswer: "console.log",
+  },
+];
 
 
 function createQuiz() {
-
+  introduction.classList.add('d-none')
+  quiz.classList.remove('d-none')
+  answerButtons.forEach(function(button){
+    console.log(button)
+    button.addEventListener("click", showNextQuestion)
+    })
+}
+function showNextQuestion(params) {
+  currentQuestionIndex++
+  question.innerText = questions[currentQuestionIndex].question
+  answerButtons.forEach(function(button, index) {
+    button.innerText = questions[currentQuestionIndex].answers[index]
+    })
 }
 
 function showResults() {
-    
+  
 }
 
-createQuiz();
 
-submitQuiz.addEventListener('click', showResults);
 
-const questions = [
-    {
-        question: 'How many data-types are there?',
-        answers: {
-            1:'2',
-            2:'3',
-            3:'4',
-            4:'5',
-        },
-        correctAnswer:'5'
-    },
-    {
-        question: 'HWhat is the true/false dattaype named?',
-        answers: {
-            1:'String',
-            2:'Boolean',
-            3:'Number',
-            4:'Test',
-        },
-        correctAnswer:'Boolean'
-    },
-    {
-        question: 'The condition in an if/else statement is enclosed with a _____.',
-        answers: {
-            1:'Quotes',
-            2:'Curly Brackets',
-            3:'Parenthesis',
-            4:'Square Brackets',
-        },
-        correctAnswer:'Curly Brackets'
-    },
-    {
-        question: 'Which method outputs a message to the web console and is also used for debugging?',
-        answers: {
-            1:'console.log',
-            2:'console.var',
-            3:'console.test',
-            4:'console.bug',
-        },
-        correctAnswer:'console.log',
-    }
-]
 
+startQuiz.addEventListener("click", createQuiz)
